73754d3ae19d0495d5ab324152d197130e698db6
Federico <fechee67@gmail.com>
2020-09-27 12:24:58 -0300

    configurando para que funcione el envio de mail

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = 'fede.amico98@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "Enviado desde la pagina de proyecto Codo a Codo";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script> alert('correo enviado exitosamente') </script>";
+    echo "<script> setTimeout(\"location.href='index.html'\" , 1000)</script>";
+?>
